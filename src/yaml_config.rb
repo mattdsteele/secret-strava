0b28ba32a7db1c2dbbcb4aba519aae4728999127
mattdsteele <orphum@gmail.com>
2020-06-10 09:42:52 -0500

    Splat operator

--- src/yaml_config.rb
+++ src/yaml_config.rb
@@ -5,14 +5,9 @@ module SecretStrava
       @config = YAML.load_file('config.yml')
     end
 
-    def method_missing(m, *args)
+    def method_missing(m)
       args = m.to_s.split '_'
-      config_state = @config
-      while args.length > 0 && config_state != nil
-        next_val = args.shift
-        config_state = config_state[next_val]
-      end
-      config_state
+      @config.send :dig, *args
     end
   end
 end
